ac4dda7e71310f4608db69756f1d69bf50a10a18
YourName <macgregoray@gmail.com>
2023-01-17 21:17:19 -0800

    incorporate jjmack code

---
+++ photoshop.js
@@ -0,0 +1,53 @@
+var fs = require("fs");
+
+// activeDov is the Photoshop file
+var activeDoc = app.activeDocument;
+
+// point to the Smart Object layer to update in the existing file
+var smartObjLayer = activeDoc.layers.filter(
+  (item) => item.kind == "LayerKind.SMARTOBJECT"
+);
+
+// point to the image to add to smartObjLayer
+var imgPath = "path/to/img";
+var imageToAdd = new File(imgPath);
+
+psdOpts = new PhotoshopSaveOptions();
+psdOpts.embedColorProfile = true;
+psdOpts.alphaChannels = true;
+psdOpts.layers = true;
+psdOpts.spotColors = true;
+
+// images to iteratively add to Smart Object layer
+var imgFolder = "path/to/img";
+var files = fs.readdirSync(imgFolder);
+
+if (files) {
+  for (var i = 0; i < files.length; i++) {
+    smartObjLayer = replaceContents(files, smartObjLayer);
+    var newName = files.name.match(/(.*)\.[^\.]+$/)[1];
+    var jpgOpts = new JPEGSaveOptions();
+    jpgOpts.quality = 10;
+    jpgOpts.embedColorProfile = true;
+    jpgOpts.matte = MatteType.NONE;
+
+    var toFile = new File(imgFolder + "/" + newName + ".jpg");
+    activeDoc.saveAs(toFile, jpgOpts, true);
+  }
+}
+
+////// replace contents //////
+
+function replaceContents(newFile, smartObj) {
+  app.activeDocument.activeLayer = smartObj;
+  var idplacedLayerReplaceContents = stringIDToTypeID(
+    "placedLayerReplaceContents"
+  );
+  var desc3 = new ActionDescriptor();
+  var idnull = charIDToTypeID("null");
+  desc3.putPath(idnull, new File(newFile));
+  var idPgNm = charIDToTypeID("PgNm");
+  desc3.putInteger(idPgNm, 1);
+  executeAction(idplacedLayerReplaceContents, desc3, DialogModes.NO);
+  return app.activeDocument.activeLayer;
+}
