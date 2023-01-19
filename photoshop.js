6d568a78d6575a1880498b4f6a2ea1ca4ca3e937
YourName <macgregoray@gmail.com>
2023-01-18 20:16:29 -0800

    corrected layer indexing

--- photoshop.js
+++ photoshop.js
@@ -1,57 +1,37 @@
-// much of this code is inspired or provided by JJMack:
-// https://community.adobe.com/t5/photoshop-ecosystem-discussions/
-// batch-replace-smart-objects-v2/m-p/10324546/page/2#M227772
-
-var fs = require("fs");
-
 // activeDov is the Photoshop file
 var activeDoc = app.activeDocument;
 
 // point to the Smart Object layer to update in the existing file
-var smartObjLayer = activeDoc.layers.filter(
-  (item) => item.kind == "LayerKind.SMARTOBJECT"
-);
+var smartObjLayer = activeDoc.layers[0];
 
 // point to the image to add to smartObjLayer
-var imgPath = "path/to/img";
-var imageToAdd = new File(imgPath);
+var imgPath = "path/to/file";
+var myFile = new File(imgPath);
 
-psdOpts = new PhotoshopSaveOptions();
-psdOpts.embedColorProfile = true;
-psdOpts.alphaChannels = true;
-psdOpts.layers = true;
-psdOpts.spotColors = true;
+if (smartObjLayer.kind == "LayerKind.SMARTOBJECT") {
+  psdOpts = new PhotoshopSaveOptions();
+  psdOpts.embedColorProfile = true;
+  psdOpts.alphaChannels = true;
+  psdOpts.layers = true;
+  psdOpts.spotColors = true;
+  smartObjLayer = replaceContents(myFile, smartObjLayer);
 
-// images to iteratively add to Smart Object layer
-var imgFolder = "path/to/img";
-var files = fs.readdirSync(imgFolder);
+  var jpgOpts = new JPEGSaveOptions();
+  jpgOpts.quality = 10;
+  jpgOpts.embedColorProfile = true;
+  jpgOpts.matte = MatteType.NONE;
 
-if (files) {
-  for (var i = 0; i < files.length; i++) {
-    smartObjLayer = replaceContents(files, smartObjLayer);
-    var newName = files.name.match(/(.*)\.[^\.]+$/)[1];
-    var jpgOpts = new JPEGSaveOptions();
-    jpgOpts.quality = 10;
-    jpgOpts.embedColorProfile = true;
-    jpgOpts.matte = MatteType.NONE;
+  var toFile = new File(imgPath + "_photoshop");
+  activeDoc.saveAs(toFile, jpgOpts, true);
 
-    var toFile = new File(imgFolder + "/" + newName + ".jpg");
-    activeDoc.saveAs(toFile, jpgOpts, true);
+  function replaceContents(newFile, theSO) {
+    app.activeDocument.activeLayer = theSO;
+    var idplacedLayerReplaceContents = stringIDToTypeID(
+      "placedLayerReplaceContents"
+    );
+    var desc244 = new ActionDescriptor();
+    desc244.putPath(charIDToTypeID("null"), new File(newFile));
+    executeAction(idplacedLayerReplaceContents, desc244, DialogModes.NO);
+    return app.activeDocument.activeLayer;
   }
-}
-
-////// replace contents //////
-
-function replaceContents(newFile, smartObj) {
-  app.activeDocument.activeLayer = smartObj;
-  var idplacedLayerReplaceContents = stringIDToTypeID(
-    "placedLayerReplaceContents"
-  );
-  var desc3 = new ActionDescriptor();
-  var idnull = charIDToTypeID("null");
-  desc3.putPath(idnull, new File(newFile));
-  var idPgNm = charIDToTypeID("PgNm");
-  desc3.putInteger(idPgNm, 1);
-  executeAction(idplacedLayerReplaceContents, desc3, DialogModes.NO);
-  return app.activeDocument.activeLayer;
 }
